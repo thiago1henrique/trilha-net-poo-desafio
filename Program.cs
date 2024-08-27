70ddbcd3c42012d899d1249af861cbf5f812ce13
Thiago <1thiago1henrique@gmail.com>
2024-08-27 14:11:06 -0300

    finalizando projeto orientação objetos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celularNokia = new Nokia("1234", "Nokia Watch", "12345", 4 );
+Iphone celularIphone = new Iphone("1234", "Nokia Watch", "12345", 4 );
+celularNokia.ReceberLigacao();
+
+celularIphone.InstalarAplicativo("Subway Suffers IOS");
+celularNokia.InstalarAplicativo("Subway Suffers Android");
